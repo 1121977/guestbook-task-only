298ce4cd7d8494a7f82c85f9e9de89ebc4a10ec6
root <root@lxc-22bab477-1bfa-483d-8959-859c6b712cf3.incus>
2026-04-16 04:55:21 +0000

    Fixed

--- src/main/java/ru/scr/security/provider/XmlAuthenticationProvider.java
+++ src/main/java/ru/scr/security/provider/XmlAuthenticationProvider.java
@@ -1,29 +1,24 @@
 package ru.scr.security.provider;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.security.authentication.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.w3c.dom.Document;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import javax.xml.namespace.QName;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathFactory;
-import java.io.File;
-import java.io.FileInputStream;
+import javax.xml.xpath.*;
 import java.io.InputStream;
-import java.net.URI;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class XmlAuthenticationProvider implements AuthenticationProvider {
 
-    final private Resource userDataResource;
+    private final Resource userDataResource;
 
     public XmlAuthenticationProvider(Resource userDataResource) {
         this.userDataResource = userDataResource;
@@ -31,26 +26,77 @@ public class XmlAuthenticationProvider implements AuthenticationProvider {
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
         String userName = authentication.getName();
         String password = authentication.getCredentials().toString();
+        
+        // Проверка на пустые учетные данные
+        if (userName == null || userName.trim().isEmpty()) {
+            throw new BadCredentialsException("Username cannot be empty");
+        }
+        if (password == null || password.isEmpty()) {
+            throw new BadCredentialsException("Password cannot be empty");
+        }
+        
         try (InputStream inputStream = userDataResource.getInputStream()) {
+            // Настройка безопасного DocumentBuilderFactory
+            DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+            builderFactory.setNamespaceAware(false);
+            builderFactory.setValidating(false);
+            
+            // Защита от XXE (XML External Entity) атак
+            builderFactory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+            builderFactory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+            builderFactory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+            builderFactory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+            builderFactory.setXIncludeAware(false);
+            builderFactory.setExpandEntityReferences(false);
+            
             DocumentBuilder builder = builderFactory.newDocumentBuilder();
             Document xmlDocument = builder.parse(inputStream);
+            
             XPath xPath = XPathFactory.newInstance().newXPath();
-            String expression = "//User[UserName/text()='" + userName + "' and" + " Password/text()='" + password + "']";
+            
+            // Использование параметризованного запроса для защиты от XPath Injection
+            String expression = "//User[UserName/text()=$userName and Password/text()=$password]";
+            
+            // Создаем карту переменных
+            Map<String, String> variables = new HashMap<>();
+            variables.put("userName", userName);
+            variables.put("password", password);
+            
+            // Устанавливаем resolver для переменных
+            xPath.setXPathVariableResolver(new XPathVariableResolver() {
+                @Override
+                public Object resolveVariable(QName variableName) {
+                    String name = variableName.getLocalPart();
+                    return variables.get(name);
+                }
+            });
+            
+            // Выполняем XPath запрос
             NodeList nodeList = (NodeList) xPath.compile(expression).evaluate(xmlDocument, XPathConstants.NODESET);
-            if (nodeList.getLength() == 0) {
-                throw new BadCredentialsException("Password is incorrect");
+            
+            // Проверяем результат
+            if (nodeList == null || nodeList.getLength() == 0) {
+                throw new BadCredentialsException("Invalid username or password");
             }
+            
+            // Дополнительная проверка - должен быть найден ровно один пользователь
+            if (nodeList.getLength() > 1) {
+                throw new AuthenticationServiceException("Multiple users found with same credentials");
+            }
+            
         } catch (BadCredentialsException e) {
             throw e;
+        } catch (AuthenticationServiceException e) {
+            throw e;
         } catch (Exception e) {
-            e.printStackTrace();
+            // Не раскрываем детали системных ошибок пользователю
+            throw new AuthenticationServiceException("Authentication failed due to system error", e);
         }
-
-        Authentication resultAuthentication = new UsernamePasswordAuthenticationToken(authentication.getPrincipal(), authentication.getCredentials(), new ArrayList<>());
-        return resultAuthentication;
+        
+        // Создаем аутентифицированный токен
+        return new UsernamePasswordAuthenticationToken(userName, null, new ArrayList<>());
     }
 
     @Override
