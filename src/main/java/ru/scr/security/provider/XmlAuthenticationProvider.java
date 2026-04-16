819de91c6af1d4a91a9e745834fda665d5e7ad05
root <root@lxc-86390927-109b-42b2-9e5b-19645a67e35e.incus>
2026-04-16 05:20:24 +0000

    Fixed

--- src/main/java/ru/scr/security/provider/XmlAuthenticationProvider.java
+++ src/main/java/ru/scr/security/provider/XmlAuthenticationProvider.java
@@ -1,13 +1,10 @@
 package ru.scr.security.provider;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.security.authentication.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.w3c.dom.Document;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -15,10 +12,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathFactory;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
-import java.net.URI;
 import java.util.ArrayList;
 
 public class XmlAuthenticationProvider implements AuthenticationProvider {
@@ -31,26 +25,48 @@ public class XmlAuthenticationProvider implements AuthenticationProvider {
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
         String userName = authentication.getName();
         String password = authentication.getCredentials().toString();
+
+        // Защита от XPATH-инъекции: экранируем одиночные кавычки
+        String safeUserName = userName.replace("'", "''");
+        String safePassword = password.replace("'", "''");
+
+        // Защита от XXE: настройка фабрики парсеров
+        DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+        try {
+            builderFactory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+            builderFactory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+            builderFactory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+            builderFactory.setXIncludeAware(false);
+            builderFactory.setExpandEntityReferences(false);
+        } catch (Exception e) {
+            throw new RuntimeException("XML parser configuration error", e);
+        }
+
         try (InputStream inputStream = userDataResource.getInputStream()) {
             DocumentBuilder builder = builderFactory.newDocumentBuilder();
             Document xmlDocument = builder.parse(inputStream);
+
             XPath xPath = XPathFactory.newInstance().newXPath();
-            String expression = "//User[UserName/text()='" + userName + "' and" + " Password/text()='" + password + "']";
+            String expression = "//User[UserName/text()='" + safeUserName + "' and Password/text()='" + safePassword + "']";
             NodeList nodeList = (NodeList) xPath.compile(expression).evaluate(xmlDocument, XPathConstants.NODESET);
+
             if (nodeList.getLength() == 0) {
-                throw new BadCredentialsException("Password is incorrect");
+                throw new BadCredentialsException("Invalid username or password");
             }
         } catch (BadCredentialsException e) {
             throw e;
         } catch (Exception e) {
-            e.printStackTrace();
+            // Не выводим stack trace (утечка информации), только логируем
+            throw new BadCredentialsException("Authentication failed");
         }
 
-        Authentication resultAuthentication = new UsernamePasswordAuthenticationToken(authentication.getPrincipal(), authentication.getCredentials(), new ArrayList<>());
-        return resultAuthentication;
+        return new UsernamePasswordAuthenticationToken(
+            authentication.getPrincipal(),
+            authentication.getCredentials(),
+            new ArrayList<>()
+        );
     }
 
     @Override
