6a5af6557acc8f21549c0c25c7215149f2098845
root <root@lxc-28bb9b22-a9a8-4627-a867-6a80d91456e8.incus>
2026-04-16 05:05:35 +0000

    Fixed

--- src/main/java/ru/scr/security/provider/XmlAuthenticationProvider.java
+++ src/main/java/ru/scr/security/provider/XmlAuthenticationProvider.java
@@ -32,25 +32,62 @@ public class XmlAuthenticationProvider implements AuthenticationProvider {
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+        try {
+            // Disable XXE
+            builderFactory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+            builderFactory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+            builderFactory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+            builderFactory.setExpandEntityReferences(false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         String userName = authentication.getName();
-        String password = authentication.getCredentials().toString();
+        String password = (authentication.getCredentials() != null) ? authentication.getCredentials().toString() : "";
+        
         try (InputStream inputStream = userDataResource.getInputStream()) {
             DocumentBuilder builder = builderFactory.newDocumentBuilder();
             Document xmlDocument = builder.parse(inputStream);
             XPath xPath = XPathFactory.newInstance().newXPath();
-            String expression = "//User[UserName/text()='" + userName + "' and" + " Password/text()='" + password + "']";
-            NodeList nodeList = (NodeList) xPath.compile(expression).evaluate(xmlDocument, XPathConstants.NODESET);
-            if (nodeList.getLength() == 0) {
-                throw new BadCredentialsException("Password is incorrect");
+            
+            // To prevent XPath injection, we can fetch all users and check them in Java
+            // or use a more complex XPath with variable resolvers. 
+            // For a small file like users.xml, manual iteration is safe and efficient.
+            NodeList nodeList = (NodeList) xPath.compile("//User").evaluate(xmlDocument, XPathConstants.NODESET);
+            
+            boolean authenticated = false;
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node userNode = nodeList.item(i);
+                String currentUserName = "";
+                String currentPassword = "";
+                
+                NodeList children = userNode.getChildNodes();
+                for (int j = 0; j < children.getLength(); j++) {
+                    Node child = children.item(j);
+                    if ("UserName".equals(child.getNodeName())) {
+                        currentUserName = child.getTextContent();
+                    } else if ("Password".equals(child.getNodeName())) {
+                        currentPassword = child.getTextContent();
+                    }
+                }
+                
+                if (userName.equals(currentUserName) && password.equals(currentPassword)) {
+                    authenticated = true;
+                    break;
+                }
+            }
+
+            if (!authenticated) {
+                throw new BadCredentialsException("Invalid username or password");
             }
         } catch (BadCredentialsException e) {
             throw e;
         } catch (Exception e) {
             e.printStackTrace();
+            throw new AuthenticationServiceException("Error during XML authentication", e);
         }
 
-        Authentication resultAuthentication = new UsernamePasswordAuthenticationToken(authentication.getPrincipal(), authentication.getCredentials(), new ArrayList<>());
-        return resultAuthentication;
+        return new UsernamePasswordAuthenticationToken(authentication.getPrincipal(), authentication.getCredentials(), new ArrayList<>());
     }
 
     @Override
