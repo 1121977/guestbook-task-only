5c540c4366d174f433ca1f608557106db74eab4b
root <root@lxc-a819d177-48c3-4b5e-9879-fe46931e11bf.incus>
2026-04-16 06:36:25 +0000

    Fixed

--- src/main/java/ru/scr/security/provider/XmlAuthenticationProvider.java
+++ src/main/java/ru/scr/security/provider/XmlAuthenticationProvider.java
@@ -1,24 +1,22 @@
 package ru.scr.security.provider;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.security.authentication.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.w3c.dom.Document;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import javax.xml.XMLConstants;
+import javax.xml.namespace.QName;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathFactory;
-import java.io.File;
-import java.io.FileInputStream;
+import javax.xml.xpath.XPathVariableResolver;
 import java.io.InputStream;
-import java.net.URI;
 import java.util.ArrayList;
 
 public class XmlAuthenticationProvider implements AuthenticationProvider {
@@ -31,26 +29,49 @@ public class XmlAuthenticationProvider implements AuthenticationProvider {
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
         String userName = authentication.getName();
         String password = authentication.getCredentials().toString();
+
         try (InputStream inputStream = userDataResource.getInputStream()) {
+            DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+            builderFactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+            builderFactory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+            builderFactory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+            builderFactory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+            builderFactory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+            builderFactory.setXIncludeAware(false);
+            builderFactory.setExpandEntityReferences(false);
+
             DocumentBuilder builder = builderFactory.newDocumentBuilder();
             Document xmlDocument = builder.parse(inputStream);
+
             XPath xPath = XPathFactory.newInstance().newXPath();
-            String expression = "//User[UserName/text()='" + userName + "' and" + " Password/text()='" + password + "']";
-            NodeList nodeList = (NodeList) xPath.compile(expression).evaluate(xmlDocument, XPathConstants.NODESET);
+            xPath.setXPathVariableResolver(new XPathVariableResolver() {
+                @Override
+                public Object resolveVariable(QName variableName) {
+                    if ("userName".equals(variableName.getLocalPart())) {
+                        return userName;
+                    } else if ("password".equals(variableName.getLocalPart())) {
+                        return password;
+                    }
+                    return null;
+                }
+            });
+
+            XPathExpression expression = xPath.compile(
+                    "//User[UserName/text()=$userName and Password/text()=$password]");
+            NodeList nodeList = (NodeList) expression.evaluate(xmlDocument, XPathConstants.NODESET);
             if (nodeList.getLength() == 0) {
                 throw new BadCredentialsException("Password is incorrect");
             }
         } catch (BadCredentialsException e) {
             throw e;
         } catch (Exception e) {
-            e.printStackTrace();
+            throw new AuthenticationServiceException("Authentication failed", e);
         }
 
-        Authentication resultAuthentication = new UsernamePasswordAuthenticationToken(authentication.getPrincipal(), authentication.getCredentials(), new ArrayList<>());
-        return resultAuthentication;
+        return new UsernamePasswordAuthenticationToken(
+                authentication.getPrincipal(), authentication.getCredentials(), new ArrayList<>());
     }
 
     @Override
