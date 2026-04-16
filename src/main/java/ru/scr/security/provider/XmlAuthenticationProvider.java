35969c7dc2046d407939a2ad38697945cd8e23c7
root <root@lxc-4bb21386-0d13-4321-a058-b0cb689e748f.incus>
2026-04-16 06:37:35 +0000

    Fixed

--- src/main/java/ru/scr/security/provider/XmlAuthenticationProvider.java
+++ src/main/java/ru/scr/security/provider/XmlAuthenticationProvider.java
@@ -1,29 +1,27 @@
 package ru.scr.security.provider;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.security.authentication.*;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.w3c.dom.Document;
-import org.w3c.dom.Node;
+import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
+import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathFactory;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
-import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
 import java.util.ArrayList;
 
 public class XmlAuthenticationProvider implements AuthenticationProvider {
 
-    final private Resource userDataResource;
+    private final Resource userDataResource;
 
     public XmlAuthenticationProvider(Resource userDataResource) {
         this.userDataResource = userDataResource;
@@ -31,30 +29,78 @@ public class XmlAuthenticationProvider implements AuthenticationProvider {
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
         String userName = authentication.getName();
-        String password = authentication.getCredentials().toString();
+        String password = authentication.getCredentials() != null
+                ? authentication.getCredentials().toString()
+                : "";
+
         try (InputStream inputStream = userDataResource.getInputStream()) {
+            DocumentBuilderFactory builderFactory = createSecureDocumentBuilderFactory();
             DocumentBuilder builder = builderFactory.newDocumentBuilder();
             Document xmlDocument = builder.parse(inputStream);
-            XPath xPath = XPathFactory.newInstance().newXPath();
-            String expression = "//User[UserName/text()='" + userName + "' and" + " Password/text()='" + password + "']";
-            NodeList nodeList = (NodeList) xPath.compile(expression).evaluate(xmlDocument, XPathConstants.NODESET);
-            if (nodeList.getLength() == 0) {
-                throw new BadCredentialsException("Password is incorrect");
+
+            if (!isValidUser(xmlDocument, userName, password)) {
+                throw new BadCredentialsException("Invalid username or password");
             }
+
+            return new UsernamePasswordAuthenticationToken(
+                    authentication.getPrincipal(),
+                    authentication.getCredentials(),
+                    new ArrayList<>()
+            );
         } catch (BadCredentialsException e) {
             throw e;
         } catch (Exception e) {
-            e.printStackTrace();
+            throw new AuthenticationServiceException("Error during XML authentication", e);
         }
-
-        Authentication resultAuthentication = new UsernamePasswordAuthenticationToken(authentication.getPrincipal(), authentication.getCredentials(), new ArrayList<>());
-        return resultAuthentication;
     }
 
     @Override
     public boolean supports(Class<?> authentication) {
         return authentication == UsernamePasswordAuthenticationToken.class;
+    }
+
+    private static DocumentBuilderFactory createSecureDocumentBuilderFactory() throws Exception {
+        DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+        builderFactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+        builderFactory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+        builderFactory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+        builderFactory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+        builderFactory.setXIncludeAware(false);
+        builderFactory.setExpandEntityReferences(false);
+        return builderFactory;
+    }
+
+    private static boolean isValidUser(Document xmlDocument, String userName, String password) {
+        NodeList userNodes = xmlDocument.getElementsByTagName("User");
+
+        for (int i = 0; i < userNodes.getLength(); i++) {
+            Element userElement = (Element) userNodes.item(i);
+
+            String storedUserName = textContent(userElement, "UserName");
+            String storedPassword = textContent(userElement, "Password");
+
+            if (constantTimeEquals(userName, storedUserName)
+                    && constantTimeEquals(password, storedPassword)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static String textContent(Element element, String tagName) {
+        NodeList nodes = element.getElementsByTagName(tagName);
+        if (nodes.getLength() == 0) {
+            return "";
+        }
+        return nodes.item(0).getTextContent().trim();
+    }
+
+    private static boolean constantTimeEquals(String left, String right) {
+        return MessageDigest.isEqual(
+                left.getBytes(StandardCharsets.UTF_8),
+                right.getBytes(StandardCharsets.UTF_8)
+        );
     }
 }
